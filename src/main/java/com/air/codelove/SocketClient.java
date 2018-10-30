430f16a0e2f4bb5c3e9b6f8bc8c921ebdedd4b38
zxlei1 <zxlei1@grgbanking.com>
2018-10-30 14:51:38 +0800

    Initial commit

---
+++ src/main/java/com/air/codelove/SocketClient.java
@@ -0,0 +1,28 @@
+package com.air.codelove;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Socket客户端
+ *
+ * @author zxlei1
+ * @date 2018/10/30 14:40
+ */
+public class SocketClient {
+    public static void main(String[] args) throws IOException {
+
+        //要连接的服务端的IP地址和端口
+        String host = "127.0.0.1";
+        int port = 55533;
+        Socket socket = new Socket(host, port);
+        OutputStream ops = socket.getOutputStream();
+        String message = "Hello,Airey!";
+        ops.write(message.getBytes("utf-8"));
+        ops.close();
+        socket.close();
+
+
+    }
+}
---
+++ src/main/java/com/air/codelove/SocketServer.java
@@ -0,0 +1,40 @@
+package com.air.codelove;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Socket服务端
+ *
+ * @author zxlei1
+ * @date 2018/10/30 14:25
+ */
+public class SocketServer {
+
+    public static void main(String[] args) throws IOException {
+
+        //监听指定端口
+        int port = 55533;
+        ServerSocket server = new ServerSocket(port);
+
+        System.out.println("server一直等待连接的到来");
+        Socket socket = server.accept();
+        //建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
+        InputStream inputStream = socket.getInputStream();
+        byte[] bytes = new byte[1024];
+        int len;
+        StringBuilder sb = new StringBuilder();
+        while ((len = inputStream.read(bytes)) != -1) {
+            sb.append(new String(bytes, 0, len, "UTF-8"));
+        }
+
+        System.out.println("get message from client : " + sb);
+        inputStream.close();
+        socket.close();
+        server.close();
+
+    }
+
+}
